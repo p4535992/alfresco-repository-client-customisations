b97706f60556c1394b9fa26b173b867b828ec106
Simon White <simon.white@gmail.com>
2013-08-19 16:46:45 +0100

    For debugging / testing purposes, adding a simple webscript that just hangs around for a certain amount of time and then returns "OK"

---
+++ source/java/com/surevine/alfresco/webscript/cluster/WaitAndRespondWebscript.java
@@ -0,0 +1,60 @@
+package com.surevine.alfresco.webscript.cluster;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+
+public class WaitAndRespondWebscript  extends AbstractWebScript {
+	
+	private static final Log _logger = LogFactory.getLog(WaitAndRespondWebscript.class);
+
+	private volatile static Integer NUMBER_OF_CONCURRENT_REQUESTS=0;
+	
+	private long _waitSeconds=60l;
+	
+	public void setWaitSeconds(long ws) {
+		_waitSeconds=ws;
+	}
+
+	private synchronized void incrementCurrentRequestCount() {
+		synchronized (NUMBER_OF_CONCURRENT_REQUESTS) {
+			NUMBER_OF_CONCURRENT_REQUESTS++;
+		}
+	}
+	
+	private synchronized void decrementCurrentRequestCount() {
+		synchronized (NUMBER_OF_CONCURRENT_REQUESTS) {
+			NUMBER_OF_CONCURRENT_REQUESTS--;
+		}
+	}
+	
+	public int getNumberOfConcurrentRequests() {
+		return getNumberOfConcurrentRequests();
+	}
+
+	@Override
+	public void execute(WebScriptRequest request, WebScriptResponse response) throws IOException {
+		try {
+			incrementCurrentRequestCount();
+			_logger.info("Starting "+WaitAndRespondWebscript.class+". "+getNumberOfConcurrentRequests()+" now active");
+			try {
+				Thread.sleep(_waitSeconds*1000l);
+			} catch (InterruptedException e) {
+				_logger.warn(WaitAndRespondWebscript.class+" interrupted!");
+			}
+			response.setStatus(200);
+			response.setContentType("text/html");
+			response.getWriter().write("<html><head><title>Alfresco</title></head><body>OK</body></html>");
+			response.getWriter().flush();
+		}
+		finally {
+			decrementCurrentRequestCount();
+			_logger.info("Finishing "+WaitAndRespondWebscript.class+". "+getNumberOfConcurrentRequests()+" now active");
+		}
+	}
+}
