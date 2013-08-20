862f3c7ab4efd0504f1915f0fd9bd8ccd0a82583
Simon White <simon.white@gmail.com>
2013-08-20 14:45:52 +0100

    Correct license headers

--- config/alfresco/templates/webscripts/org/alfresco/repository/enhanced-security/visibility/lib/visibility.lib.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/enhanced-security/visibility/lib/visibility.lib.js
@@ -1,8 +1,24 @@
-/**
- * Library file for Enhanced Security Marking / Visibility webscripts
- */
-
-
+/*
+ * Copyright (C) 2008-2010 Surevine Limited.
+ *   
+ * Although intended for deployment and use alongside Alfresco this module should
+ * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
+ * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 /**
  * Parse some page arguments and return the parameters specified therein as an object
  * Markings live in the following format  es_validOpenMarkings,OG01,OG02,OG03,OG04;es_validClosedMarkings,CG01....
--- source/java/com/surevine/alfresco/repo/upload/test/FilenameNormaliserTest.java
+++ source/java/com/surevine/alfresco/repo/upload/test/FilenameNormaliserTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008-2010 Surevine Limited.
+ *   
+ * Although intended for deployment and use alongside Alfresco this module should
+ * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
+ * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package com.surevine.alfresco.repo.upload.test;
 
 import org.junit.Before;
@@ -53,14 +74,14 @@ public class FilenameNormaliserTest {
 	public void extendedASCIIForbidden()
 	{
 		// TODO These characters aren't being mapped properly in UTF-8 under maven 3.  Can we use characters that are?
-		preserved("test¬‚«~«æ", "test");
-		removed("test¬‚«~«æ", "¬");
-		removed("test¬‚«~«æ", "‚");
-		removed("test¬‚«~«æ", "«");
-		preserved("test¬‚«~«æ", "~");
-		removed("test¬‚«~«æ", "«");
-		removed("test¬‚«~«æ", "E");
-		removed("    jfdkdghfj test¬‚«~«æ", "E");
+		preserved("testï¿½ï¿½ï¿½~ï¿½ï¿½", "test");
+		removed("testï¿½ï¿½ï¿½~ï¿½ï¿½", "ï¿½");
+		removed("testï¿½ï¿½ï¿½~ï¿½ï¿½", "ï¿½");
+		removed("testï¿½ï¿½ï¿½~ï¿½ï¿½", "ï¿½");
+		preserved("testï¿½ï¿½ï¿½~ï¿½ï¿½", "~");
+		removed("testï¿½ï¿½ï¿½~ï¿½ï¿½", "ï¿½");
+		removed("testï¿½ï¿½ï¿½~ï¿½ï¿½", "E");
+		removed("    jfdkdghfj testï¿½ï¿½ï¿½~ï¿½ï¿½", "E");
 	}
 	
 	@Test
