c435538a22dbef6c7ec5f6237e6d3f51c097700c
Jonny Heavey <jonny.heavey@surevine.com>
2014-03-25 21:07:32 +0000

    Added constant declaration / comments back into lib file (removed by license header tool). Fixes #1

--- config/alfresco/templates/webscripts/org/alfresco/repository/comments/comments.lib.js
+++ config/alfresco/templates/webscripts/org/alfresco/repository/comments/comments.lib.js
@@ -1,24 +1,32 @@
 /*
  * Copyright (C) 2008-2010 Surevine Limited.
- *   
+ *
  * Although intended for deployment and use alongside Alfresco this module should
  * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
  * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+/** Name used for the topic that contains all comments. */
+const COMMENTS_TOPIC_NAME = "Comments";
+
+/**
+ * Returns all comment nodes for a given node.
+ * @return an array of comments.
+ */
 function getComments(node)
 {
    var commentsFolder = getCommentsFolder(node);
@@ -36,7 +44,7 @@ function getComments(node)
 
 /**
  * Returns the folder that contains all the comments.
- * 
+ *
  * We currently use the fm:discussable aspect where we
  * add a "Comments" topic to it.
  */
@@ -61,8 +69,8 @@ function getOrCreateCommentsFolder(node)
 {
    var commentsFolder = getCommentsFolder(node);
    if (commentsFolder == null)
-   {    
-      commentsFolder = commentService.createCommentsFolder(node); 
+   {
+      commentsFolder = commentService.createCommentsFolder(node);
    }
    return commentsFolder;
 }
