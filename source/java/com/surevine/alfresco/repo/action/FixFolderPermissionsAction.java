de73d1a7b65f9f25a7a78465cedc1cb2a85323da
simonw2 <simon.white@gmail.com>
2014-04-15 11:12:26 +0100

    New action to clear delete permissions from a folder ref INTS-253

---
+++ source/java/com/surevine/alfresco/repo/action/FixFolderPermissionsAction.java
@@ -0,0 +1,57 @@
+package com.surevine.alfresco.repo.action;
+
+import java.util.List;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.PermissionService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class FixFolderPermissionsAction extends ActionExecuterAbstractBase {
+
+	private static final Log LOGGER = LogFactory.getLog(FixFolderPermissionsAction.class);
+
+	private PermissionService _permissionService;
+
+	private NodeService _nodeService;
+
+	public void setPermissionService(PermissionService permissionService) {
+		_permissionService = permissionService;
+	}
+
+	public void setNodeService(NodeService nodeService) {
+		_nodeService = nodeService;
+	}
+
+	@Override
+	protected synchronized void executeImpl(final Action action, final NodeRef nodeRef) {
+
+		if (nodeRef != null && _nodeService.exists(nodeRef)) {
+			final boolean isFolder = _nodeService.getType(nodeRef).equals(ContentModel.TYPE_FOLDER);
+
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug(String.format("Is %s a folder?  %b", nodeRef, isFolder));
+			}
+			if (isFolder) {
+				if (LOGGER.isTraceEnabled()) {
+					LOGGER.trace(String.format("Clearing delete permissions of %s", nodeRef));
+				}
+				_permissionService.clearPermission(nodeRef, PermissionService.DELETE);
+				if (LOGGER.isInfoEnabled()) {
+					LOGGER.info(String.format("Cleared delete permissions of %s", nodeRef));
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void addParameterDefinitions(List<ParameterDefinition> arg0) { // Intentionally
+																				// left
+																				// blank
+	}
+}
